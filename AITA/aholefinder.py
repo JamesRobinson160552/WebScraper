56d0a674f216559a2f85f1fb8b2e68b759230b5f
James Robinson <160552r@acadiau.ca>
2023-12-03 21:33:26 -0400

    Handled Some Errors
    -Double quotes in the data no longer cause invalid JSON syntax
    -Errors (unencodable chars) do not crash the program or cause invalid JSON syntax

--- AITA/aholefinder.py
+++ AITA/aholefinder.py
@@ -10,11 +10,20 @@
 from bs4 import BeautifulSoup #To parse HTML
 import requests #To get HTML
 from datetime import datetime #To get date for file naming
+import traceback #To debug better
 import time
 
 filename = datetime.today().strftime('%Y-%m-%d') + '.json'
 with open('./data/' + filename, 'w') as f:
     f.write("[")
+
+#Ensures text pulled from posts is compatible with JSON
+def clean_text(text):
+    """Adds escape characters to double quotes.
+    Removes leading and trailing whitespace."""
+    clean_text = text.strip()
+    clean_text = clean_text.replace('"', '\\"')
+    return clean_text
 
 #Analyze Post Function-----------------------------------------------------------------------------
 def analyze_post(post_link):
@@ -22,39 +31,48 @@ def analyze_post(post_link):
     Get the title, post body, and public decision.
     Store the above in a text file."""
 
+    #Get post
     post = requests.get(post_link)
     postsoup = BeautifulSoup(post.text, 'lxml')
 
-    title = postsoup.find('h1').text.strip()
+    #Parse for relevant data
+    title = clean_text(postsoup.find('h1').text)
     paragraphs = postsoup.find_all('p')
     body = ""
     for paragraph in paragraphs[1:]:
-        body += paragraph.text.strip()
-    decision = postsoup.find('shreddit-post-flair').text.strip()
+        body += clean_text(paragraph.text)
+    decision = clean_text(postsoup.find('shreddit-post-flair').text)
 
     #Store results in a JSON file
-    filename = datetime.today().strftime('%Y-%m-%d') + '.json'
-    with open('./data/' + filename, 'a') as f:
-        f.write("\n{\n\t\"title\" : \"" + title + "\",")
-        f.write("\n\t\"body\" : \"" + body + "\",")
-        f.write("\n\t\"decision\" : \"" + decision + "\"\n},")
-
     print (title)
     print (body)
     print (decision)
 
-#Main----------------------------------------------------------------------------------------------
-#test_post = 'https://www.reddit.com/r/AmItheAsshole/comments/17yohi6/aita_for_eating_at_the_children_i_babysit_fors/'
-#analyze_post(test_post)
+    filename = datetime.today().strftime('%Y-%m-%d') + '.json'
+    with open('./data/' + filename, 'a') as f:
+        try:
+            f.write("\n{\n\t\"title\" : \"" + title + "\",")
+            f.write("\n\t\"body\" : \"" + body + "\",")
+            f.write("\n\t\"decision\" : \"" + decision + "\",")
+            f.write("\n\t\"valid\" : \"true\" \n},")
+        except: #This takes care of crash-causing edge cases, mostly unencodable chars atm
+            f.write("\n\t\"valid\" : \"false\" \n},") #Since exceptions occur in the body typically, this maintains the JSON file format
+            print("")
+            traceback.print_exc()
+            print("")
 
+#Main----------------------------------------------------------------------------------------------
 home = requests.get('https://www.reddit.com/svc/shreddit/community-more-posts/top/?t=WEEK&after=dDNfMTgyaGVnYg%3D%3D&name=AmItheAsshole&adDistance=3&feedLength=25')
 homesoup = BeautifulSoup(home.text, 'lxml')
 posts = homesoup.find_all('a', slot='full-post-link')
 
 print (len(posts))
-print (posts[0]['href'])
+print (posts[0]['href'])\
+
 for post in posts:
     analyze_post('https://www.reddit.com' + post['href'])
-f.write("\n]")
 
+with open('./data/' + filename, 'a') as f:
+    f.write("\n]\n")
 
+f.close()
